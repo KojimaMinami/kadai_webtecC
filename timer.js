08be0c19d79daa733c32f07b0f440c7df7a9f8b7
KojimaMinami <104058345+KojimaMinami@users.noreply.github.com>
2022-11-12 10:34:00 +0900

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,21 +1,40 @@
-function clock() {
-    var now = new Date();
-    var hour = now.getHours(); // 時
-    var min = now.getMinutes(); // 分
-    var sec = now.getSeconds(); // 秒
+/* 全体を囲うコンテナー */
 
-    // 数値が1桁の場合、頭に0を付けて2桁で表示する指定
-    if (hour < 10) { hour = "0" + hour; }
-    if (min < 10) { min = "0" + min; }
-    if (sec < 10) { sec = "0" + sec; }
-
-    // フォーマット①
-    var clock = ' ' + hour + ': ' + min + ': ' + sec + ' ';
-
-    document.getElementById('RealtimeClockArea').innerHTML = clock.toLocaleString(); // div id="clock-01"
-
-
-    // 1000ミリ秒ごとに処理を実効
-    window.setTimeout("clock()", 1000);
+body {
+    width: 100%;
+    height: 100vh;
+    background-color: #BF6956;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
 }
-window.onload = clock;
+
+
+/* 時計の共通スタイル */
+
+#RealtimeClockArea {
+    font-family: 'Share Tech Mono', monospace;
+    color: #3F4659;
+    text-align: center;
+    font-size: 45px;
+}
+
+.text {
+    color: #494597;
+    font-size: 40px;
+}
+
+
+/* 日付のスタイル */
+
+.clock-date {
+    font-size: 25px;
+}
+
+
+/* 時間のスタイル */
+
+.clock-time {
+    font-size: 100px;
+}
